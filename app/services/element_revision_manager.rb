ff35b29934e39c8057b688f7e7039a80ae351749
ohkubo kohei <ohkubo@heartrails.com>
2026-02-22 11:23:36 +0900

    Sync element revisions and prune orphans

--- app/services/element_revision_manager.rb
+++ app/services/element_revision_manager.rb
@@ -20,7 +20,25 @@ class ElementRevisionManager
     revision = @element.element_revisions.build(@revision_params)
     revision.user = @user
     revision.revision = number
-    revision.save
+    if revision.save
+      update_owned_plot_elements(revision)
+      prune_unreferenced_revisions(revision.id)
+    end
     revision
+  end
+
+  def update_owned_plot_elements(revision)
+    PlotElement.joins(:plot)
+               .where(element_id: @element.id, plots: { user_id: @user.id })
+               .update_all(element_revision_id: revision.id, updated_at: Time.current)
+  end
+
+  def prune_unreferenced_revisions(current_revision_id)
+    ElementRevision
+      .where(element_id: @element.id)
+      .where.not(id: current_revision_id)
+      .left_joins(:plot_elements)
+      .where(plot_elements: { id: nil })
+      .delete_all
   end
 end
--- test/services/element_revision_manager_test.rb
+++ test/services/element_revision_manager_test.rb
@@ -4,6 +4,9 @@ class ElementRevisionManagerTest < ActiveSupport::TestCase
   def setup
     @user = User.create!(name: "Author")
     @element = Element.create!(user: @user, element_type: "Character", name: "Hero")
+    @scene = Scene.create!(user: @user, text: "Scene")
+    @plot = Plot.create!(user: @user, title: "Plot", scene: @scene)
+    PlotSceneLink.create!(plot: @plot, scene: @scene, next_scene: nil)
   end
 
   test "creates initial revision" do
@@ -21,5 +24,15 @@ class ElementRevisionManagerTest < ActiveSupport::TestCase
 
     assert revision.persisted?
     assert_equal 2, revision.revision
+  end
+
+  test "updates owned plot elements to latest revision" do
+    initial = ElementRevisionManager.new(element: @element, user: @user, revision_params: { summary: "Lead" }).create_initial
+    plot_element = PlotElement.create!(plot: @plot, element: @element, element_revision: initial)
+
+    updated = ElementRevisionManager.new(element: @element, user: @user, revision_params: { summary: "Update" }).create_next
+
+    plot_element.reload
+    assert_equal updated.id, plot_element.element_revision_id
   end
 end
