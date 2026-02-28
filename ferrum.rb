bbd8b00b8335a55d6d83a2c0f4f5581527e42443
Ohkubo KOHEI <o@kbn.one>
2026-02-28 14:27:37 +0000

    ferrum test

--- ferrum.rb
+++ ferrum.rb
@@ -1,5 +1,19 @@
 require "ferrum"
-browser = Ferrum::Browser.new(browser_options: { 'no-sandbox': nil })
+
+browser = Ferrum::Browser.new(
+	browser_path: ENV["BROWSER_PATH"],
+	process_timeout: ENV.fetch("FERRUM_PROCESS_TIMEOUT", 30).to_i,
+	timeout: ENV.fetch("FERRUM_DEFAULT_TIMEOUT", 15).to_i,
+	browser_options: {
+		'no-sandbox': nil,
+		'disable-setuid-sandbox': nil,
+		'disable-dev-shm-usage': nil,
+		'disable-gpu': nil,
+		'no-first-run': nil,
+		'no-default-browser-check': nil,
+		'headless': 'new'
+	}
+)
 browser.go_to("https://google.com")
 browser.screenshot(path: "tmp/screenshots/google.png")
 browser.quit
