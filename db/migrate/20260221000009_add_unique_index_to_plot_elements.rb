5d16c76138d141db065e3ff9328ded0286cc827f
Ohkubo KOHEI <o@kbn.one>
2026-03-01 09:30:40 +0000

    rm migrations

--- db/migrate/20260221000009_add_unique_index_to_plot_elements.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueIndexToPlotElements < ActiveRecord::Migration[8.0]
-  def change
-    add_index :plot_elements, [ :plot_id, :element_id ], unique: true
-  end
-end
--- db/migrate/20260228142457_add_user_id_to_chats.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToChats < ActiveRecord::Migration[8.1]
-  def change
-    add_reference :chats, :user, null: false, foreign_key: true
-  end
-end
